07989cdc0fa738bbf4296845a787b8b623e6806a
soft <soft@stu25c6ed>
2015-10-26 10:17:46 +0800

    JudgementBLService的驱动

---
+++ code/src/bl/blservice/JudgementBLService.java
@@ -0,0 +1,21 @@
+package bl.blservice;
+
+import po.OrderBillPO;
+
+public interface JudgementBLService {
+	
+	public ResultMessage checkRecord(OrderBillPO record){
+		
+	}
+	
+	public ResultMessage updateRecord(OrderBillPO record){
+		
+	}
+	
+	public ResultMessage submitRecord(OrderBillPO record){
+		
+	}
+
+
+
+}
