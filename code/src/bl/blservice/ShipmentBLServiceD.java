c24fc1cb69a82686b3c9b00cf60d7d7cbae8c08b
soft <soft@stu25c6ed>
2015-10-26 11:38:17 +0800

    JudgementBL的驱动以及改动

---
+++ code/src/bl/blservice/ShipmentBLServiceD.java
@@ -0,0 +1,28 @@
+package bl.blservice;
+
+import bl.blservice.ShipmentBLService;
+
+public class ShipmentBLServiceD  {
+	//shipmentbl的供接口
+		public void drive(ShipmentBLService shipmentBLService){
+			ResultMessage result1 = shipmentBLService.getTruckID();
+			if(result1==ResultMessage.Exist)System.out.println("TruckID exists\n");
+			ResultMessage result2 = shipmentBLService.getFreightID(0000000001);
+			if(result2==ResultMessage.Exist)System.out.println("FreightID exists\n");
+			ResultMessage result3 = shipmentBLService.getReceiveID(0000000001);
+			if(result3==ResultMessage.Exist)System.out.println("ReceiveID exists\n");
+			ResultMessage result4 = shipmentBLService.buildShipment();
+			if(result4==ResultMessage.Exist)System.out.println("buildShipmentBill exists\n");
+			ResultMessage result5 = shipmentBLService.save((ShipmentInputInfo info));
+			if(result5==ResultMessage.Exist)System.out.println("ShipmentBill exists\n");
+		}
+
+	}
+
+	public class Client{
+		public static void main(String[] args){
+			ShipmentBLService shipmentController = new ShipmentController();
+			ShipmentBLServiceD driver = new ShipmentBLServiceD(shipmentController);
+			driver.drive(shipmentController);
+		}
+	}
