3262973be3d40bdfdf02cfe01d321dc62989c5e5
nch14 <nch14@software.nju.edu.cn>
2016-01-05 10:46:24 +0800

    解决了一些界面的监听问题

--- code/src/bill/ArrivementBill_Center.java
+++ code/src/bill/ArrivementBill_Center.java
@@ -6,7 +6,7 @@ public class ArrivementBill_Center extends ArrivementBill{
 	 * 
 	 */
 	private static final long serialVersionUID = -5512259518031962786L;
-	String centerID;
+	public String centerID;
 	
 	public ArrivementBill_Center(String[] time,String ID,String centerID){
 		this.centerID=centerID;
